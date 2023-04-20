7b80ec853a50f49505edc8632bedd9940420f808
Ashwin.D.S <astroashwin@outlook.com>
2023-04-20 03:27:31 +0000

    official_devices: scripts: upload DTBO as well Also while we are at it, add the device name in the filenames

--- .github/scripts/Uploader/__main__.py
+++ .github/scripts/Uploader/__main__.py
@@ -72,15 +72,25 @@ for tag in new_tags:
         except Exception as e:
             print (e)
     recovery_path = ""
+    mDevice = ""
+    device = ""
+    for file in os.listdir(cur_dir + "/releases/"):
+        if file.endswith(".json"):
+            device = file.replace(".json", "")
+            mDevice = device + "-"
     for file in os.listdir(cur_dir + "/releases"):
+        # TODO: All the following if conditions can be made much simpler instead of 69 cases
         if file == "vendor_boot.img":
-            os.system("mv " + cur_dir + "/releases/vendor_boot.img " + cur_dir + "/releases/vendor_boot-" +
+            os.system("mv " + cur_dir + "/releases/vendor_boot.img " + cur_dir + "/releases/vendor_boot-" + mDevice +
                       tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
         if file == "boot.img":
-            os.system("mv " + cur_dir + "/releases/boot.img " + cur_dir + "/releases/boot-" +
+            os.system("mv " + cur_dir + "/releases/boot.img " + cur_dir + "/releases/boot-" + mDevice +
                       tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
         if file == "recovery.img":
-            os.system("mv " + cur_dir + "/releases/recovery.img " + cur_dir + "/releases/recovery-" +
+            os.system("mv " + cur_dir + "/releases/recovery.img " + cur_dir + "/releases/recovery-" + mDevice +
+                      tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
+        if file == "dtbo.img":
+            os.system("mv " + cur_dir + "/releases/dtbo.img " + cur_dir + "/releases/dtbo-" + mDevice +
                       tag.split("_")[0] + "-" + str(datetime.date.today()).replace("-", "") + ".img")
     print("Downloaded")
     os.chdir(cur_dir + "/releases-public")
