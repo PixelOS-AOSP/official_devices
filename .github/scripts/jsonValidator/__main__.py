2e87a5b1f14611346e7daef9e51b49e84c4cfcd7
Ashwin.D.S <astroashwin@outlook.com>
2023-05-11 06:48:27 +0000

    docs: add and auto-update Device List
    Signed-off-by: Ashwin.D.S <astroashwin@outlook.com>

--- .github/scripts/jsonValidator/__main__.py
+++ .github/scripts/jsonValidator/__main__.py
@@ -44,3 +44,52 @@ for folder in foldersToFormat:
             else:
                 print("json error")
                 raise SystemExit("invalid json")
+
+
+# Device List
+
+headerText = "# Device List\nHere is the list of all the devices actively supprorted officially by PixelOS. To download the latest version of PixelOS, please visit our official website at [PixelOS.net](PixelOS.net), there you can find the necessary resources and information to download and install PixelOS on your device."
+footerText = "We hope you enjoyed the project! Your donations help us maintain our infrastructure and continue our work. Please consider showing your support by donating! [Click Me](https://wiki.pixelos.net/docs/donate)"
+
+ReadmeText = headerText + "\n\nNumber Of Devices in Official PixelOS: <**TotalCountGoesHere**>"
+
+officialDeviceList = json.loads(open("API/devices.json", "r").read())
+
+brands = []
+
+for device in officialDeviceList["devices"]:
+    if device["vendor"] not in brands and device["active"]:
+        brands.append(device["vendor"])
+
+brands = sorted(brands)
+brandDevices = {}
+
+totalCount = 0
+
+for brand in brands:
+    brandDevices [brand] = []
+    for device in officialDeviceList["devices"]:
+        if device["vendor"] == brand and device["active"]:
+            brandDevices [brand].append(device["model"])
+            totalCount += 1
+    brandDevices [brand] = sorted(brandDevices [brand])
+
+
+print (brandDevices)
+
+ReadmeText = ReadmeText + "\n\nNumber of Brands in Official PixelOS: " + str(len(brands))
+
+
+ReadmeText = ReadmeText + "\n\nOfficially Supported Devices:"
+
+for brand in brands:
+    ReadmeText = ReadmeText + "\n\n**" + brand + "**"
+    count = 1
+    for branddevice in brandDevices[brand]:
+        for device in officialDeviceList["devices"]:
+            if device["model"] == branddevice:
+                ReadmeText = ReadmeText + "\n" + str(count) + ". " + device["model"] + " (" + device["codename_alt"] + ")"
+                count +=1
+
+
+open("docs/DeviceList.md", "w+").write(ReadmeText.replace("<**TotalCountGoesHere**>", str(totalCount)) + "\n\n" + footerText)
